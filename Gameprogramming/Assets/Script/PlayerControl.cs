f6e72b354e6ebe6e58d7eb93a537ee36c29b9e22
Simon <simon.louis638@gmail.com>
2024-02-19 09:58:11 +0100

    new movement + jump in progress

--- Gameprogramming/Assets/Script/PlayerControl.cs
+++ Gameprogramming/Assets/Script/PlayerControl.cs
@@ -1,46 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    private CustomInput input = null;
-    private Vector2 moveVector = Vector2.zero;
-    private Rigidbody2D rb = null;
-    private float moveSpeed = 10.0f;
-    private void Awake()
+    public Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    public float jumpForce = 5.0f;
+
+    void Update()
     {
-        input = new CustomInput();
-        rb = GetComponent<Rigidbody2D>();
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput, 0f);
+
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
     }
 
-    private void OnEnable()
+    void Jump()
     {
-        input.Enable();
-        input.Player1.Movement.performed += OnMovementPerformed;
-        input.Player1.Movement.canceled += OnMovementCanceled;
-    }
-
-    private void OnDisable()
-    {
-        input.Disable();
-        input.Player1.Movement.performed -= OnMovementPerformed;
-        input.Player1.Movement.canceled -= OnMovementCanceled;
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = moveVector * moveSpeed;
-    }
-
-    private void OnMovementPerformed(InputAction.CallbackContext value)
-    {
-        moveVector = value.ReadValue<Vector2>();
-    }
-
-    private void OnMovementCanceled(InputAction.CallbackContext value)
-    {
-        moveVector = Vector2.zero;
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
