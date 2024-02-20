aa013e50be976f0b5fcc6626f940dd2491a6f645
L0uxe <Loulourxl@hotmail.com>
2024-02-20 07:58:25 +0100

    menu terminer

--- Gameprogramming/Assets/Script/Menu.cs
+++ Gameprogramming/Assets/Script/Menu.cs
@@ -6,6 +6,7 @@ public class MenuManager : MonoBehaviour
     public void NouvellePartie()
     {
         SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
     }
 
     public void Options()
